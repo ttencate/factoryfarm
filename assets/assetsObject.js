0dc2d440e28c1cdf6a618f31f44dacc1a1aa9f2e
Thomas ten Cate <ttencate@gmail.com>
2017-12-02 17:40:11 +0100

    Implement farmer walk animation

--- assets/assetsObject.js
+++ assets/assetsObject.js
@@ -1,17 +1,29 @@
 var assetsObject = {
-"audio": {
-	"bgmusic": ["bgmusic.mp3", "bgmusic.ogg"],
-},
-"images": [
-	"nosound.png",
-	"chicken.png",
-	"sound.png",
-	"music.png",
-	"tileset.png",
-	"coins.png",
-	"farmer.png",
-	"grass.png",
-	"backgroundGrass.png",
-	"nomusic.png",
-	"favoicon.png"],
+	"audio": {
+		"bgmusic": ["bgmusic.mp3", "bgmusic.ogg"],
+	},
+	"images": [
+		"nosound.png",
+		"chicken.png",
+		"sound.png",
+		"music.png",
+		"tileset.png",
+		"coins.png",
+		"grass.png",
+		"backgroundGrass.png",
+		"nomusic.png",
+		"favoicon.png",
+	],
+	"sprites": {
+		"farmer.png": {
+			tile: 64,
+			tileh: 64,
+			map: {
+				farmer_down: [0, 0],
+				farmer_up: [0, 1],
+				farmer_right: [0, 2],
+				farmer_left: [0, 3],
+			},
+		}
+	},
 };
--- src/scenes/scene_Main.js
+++ src/scenes/scene_Main.js
@@ -61,8 +61,13 @@ Crafty.scene('Main', function() {
 			};
 		};
 
-		player = Crafty.e('2D, WebGL, Color, KeyControls, Moving, Collision, SpriteAnimation')
-				.color('red').attr({x: 800, y: 500, w: 50, h: 50, z: zLevels['player']})
+		player = Crafty.e('2D, WebGL, Sprite, farmer_down, KeyControls, Moving, Collision, SpriteAnimation, ReelFromVelocity')
+				.attr({x: 800, y: 500, w: 64, h: 64, z: zLevels['player']})
+				.reel('walking_down', 500, [[0, 0], [1, 0], [2, 0], [3, 0]])
+				.reel('walking_up', 500, [[0, 1], [1, 1], [2, 1], [3, 1]])
+				.reel('walking_right', 500, [[0, 2], [1, 2], [2, 2], [3, 2]])
+				.reel('walking_left', 500, [[0, 3], [1, 3], [2, 3], [3, 3]])
+				.animate('walking_down', 0)
 				._Moving()
 				._KeyControls(Crafty.keys.LEFT_ARROW, Crafty.keys.RIGHT_ARROW, Crafty.keys.UP_ARROW, Crafty.keys.DOWN_ARROW);
 		Crafty.viewport.clampToEntities = false;
